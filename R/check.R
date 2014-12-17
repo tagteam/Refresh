745080f7f7851c51b52bcb572ec33abecbbde127
tag <tag@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-17 13:08:34 +0000

    adapted to new rules
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/bubble/pkg/Refresh@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/check.R
+++ R/check.R
@@ -4,13 +4,15 @@ check <- function(pkgName,
                   LogDir=options()$refreshCheckLog,
                   lib=.libPaths()[1],
                   recursive=FALSE,
-                  devel=TRUE,
+                  devel=file.exists("~/R/dev/R-devel"),
                   ask=FALSE,as.cran=TRUE){
     pkgName <- as.character(substitute(pkgName))  
     if (is.null(Archive) || ask)
         Archive <- file.choose()
     if (is.null(Archive) || !(file.exists(Archive)))
         warning("Argument `Archive' is not a valid directory-name.\nIt should be a directory in which the `packageName_version.tar.gz' files are stored.")
+    ## make sure Archive does not end in /
+    Archive <- sapply(Archive, function(a)strsplit(file.path(a),"/$")[[1]])
     ## check for lockfile
     if (version$major>=3 || (version$major>=2 & version$minor >= 15))
         lock <- paste(lib,"/00LOCK-",pkgName,sep="")
@@ -20,8 +22,16 @@ check <- function(pkgName,
         message("Remove file:",lock)
         system(paste("rm -rf",lock),intern=FALSE)
     }
-    versions <- list.files(path=file.path(Archive),pattern=paste(pkgName,".*.tar.gz$",sep=""),recursive=recursive)
-    version <- select.list(versions,multiple=FALSE,title="Select package version: ")
+    versions <- list.files(path=file.path(Archive),
+                           pattern=paste(pkgName,".*.tar.gz$",sep=""),
+                           recursive=recursive,full.names=TRUE)
+    ## if (length(Archive)>1)
+    ## Archive <- select.list(Archive,
+    ## multiple=FALSE,
+    ## title="Select Archive: ")
+    version <- select.list(versions,
+                           multiple=FALSE,
+                           title="Select package version: ")
     if (!file.exists(LogDir)){
         cat("\nChoose a directory for the log of Rcheck (e.g. ~/tmp/) no quotes!\n")
         LogDir <- file.choose()
@@ -29,11 +39,33 @@ check <- function(pkgName,
     if (devel==TRUE)
         R <- "~/R/dev/R-devel/bin/R"
     else
-        R <- "/usr/bin/R"
+        R <- file.path(R.home(), "bin", "R")
     if (as.cran==TRUE)
-        checkstring <- paste("cd ",LogDir,";",R," CMD check --as-cran --library=",path.expand(lib)," ",path.expand(file.path(Archive)),"/",version,sep="")
+        checkstring <- paste("cd ",
+                             LogDir,
+                             ";",
+                             "export R_C_BOUNDS_CHECK='yes';",
+                             R,
+                             " CMD check --as-cran --library=",
+                             path.expand(lib),
+                             " ",
+                             ## path.expand(file.path(Archive)),
+                             ## "/",
+                             version,
+                             sep="")
     else
-        checkstring <- paste("cd ",LogDir,";",R," CMD check --library=",path.expand(lib)," ",path.expand(file.path(Archive)),"/",version,sep="")
+        checkstring <- paste("cd ",
+                             LogDir,
+                             ";",
+                             "export R_C_BOUNDS_CHECK='yes';",
+                             R,
+                             " CMD check --library=",
+                             path.expand(lib),
+                             " ",
+                             ## path.expand(file.path(Archive)),
+                             ## "/",
+                             version,
+                             sep="")
     message(checkstring)
     system(checkstring)
 }
