c424ca34e5ad3afd663a9b5f3ef596a176da1869
Thomas Alexander Gerds <tag@biostat.ku.dk>
2017-10-21 08:21:43 +0200

    now using devtools

--- R/build.R
+++ R/build.R
@@ -1,6 +1,6 @@
 ##' Build an R-package from source files.
 
-#' @title R packaging tool controlled from within the running R process
+#' @title R packaging tool combining emacs save buffers with devtools::document, devtools::build, devtools::install
 #' @param pkg  The quoted or unquoted name of the package 
 #' @param lib A directory in which to install the package. Default is
 #' \code{options()$refreshLibrary}. The directory should be in \code{.libPaths()}.
@@ -17,7 +17,7 @@
 #' @param vignettes if set to FALSE add \code{--no-vignettes} to CMD build
 #' @param verbose The level of verbosity
 #' @export
-build <- function(pkg,
+Build <- function(pkg,
                   lib=options()$refreshLibrary,
                   Archive=options()$refreshArchive,
                   Source=options()$refreshSource,
@@ -34,13 +34,10 @@ build <- function(pkg,
     pkg <- as.character(substitute(pkg))
     oldPwd <- getwd()
     setwd(file.path(Archive))
-    
-    ## if (!is.character(pkg))
-    # {{{  locating files
-    
+    # {{{ locating the package library and the archive
     if (missing(lib)) lib <- options()$refreshLibrary
     if (is.null(lib)||!(file.exists(lib)))
-        warning("Argent lib is not a valid directory-name.\nYou may create such a location e.g. by evaluating the R-command\n `dir.create(\"~/R/library\",recursive=TRUE)'")
+        stop("Argument lib does not point to an existing directory-name.")
     lib <- path.expand(lib)
     if (!(match(sub("/$","",lib),sapply(.libPaths(),function(x)sub("/$","",x)),nomatch=FALSE)))
         warning("\nThe library is not in the search path.\nYou can add it by evaluating the R-command\n `.libPaths(\"",lib,"\")'")
@@ -51,18 +48,14 @@ build <- function(pkg,
     }
     if (is.null(Archive) || !(file.exists(Archive)))
         warning("Argument `Archive' is not a valid directory-name.\nIt should be a directory in which the `packageName_version.tar.gz' files are stored.")
-
     # }}}
-    # {{{  search for an uncompressed package directory with the source code
-
+    # {{{ Search for the uncompressed package directory 
     if (is.null(Source) || ask){
         cat("\nChoose the directory where the R source code of your package lives\n ")
         Source <- file.choose()
     }
-    
     Source <- Source[!duplicated(Source)]
     found <- sapply(Source,function(s){file.exists(file.path(s,pkg,"DESCRIPTION"))})
-    ## print(file.path(Source,pkg))
     if (sum(found)>1){
         warning("Package source found in two different places.")
         Spath <- select.list(Source[found],multiple=FALSE,title="Package source found in two different places, please choose: ")
@@ -72,96 +65,82 @@ build <- function(pkg,
         if (any(found)) 
             SourceP <- file.path(Source[found],pkg)
         else
-            SourceP <- NA
+            stop("No directory with name '",pkg,"' found in:\n\n",paste(Source,"\n"))
     }
-
-    if (!is.na(SourceP)){
-        sourceCodeVersionLine <- system(paste("grep Version: ",file.path(SourceP,"DESCRIPTION")),intern=TRUE)
-        sourceCodeVersion <- strsplit(sourceCodeVersionLine,"Version: ")[[1]][[2]]
-        sourceCodeVersion <- gsub("\r","",sourceCodeVersion)
-    }
-    else{
-        ## stop if no directory with that name
-        stop("No directory with name '",
-             pkg,
-             "' found in:\n\n",
-             paste(Source,"\n"))
-    }
+    ## if (!is.na(SourceP)){
+    ## sourceCodeVersionLine <- system(paste("grep Version: ",file.path(SourceP,"DESCRIPTION")),intern=TRUE)
+    ## sourceCodeVersion <- strsplit(sourceCodeVersionLine,"Version: ")[[1]][[2]]
+    ## sourceCodeVersion <- gsub("\r","",sourceCodeVersion)
+    ## }
     # }}}
-    # {{{  
-    ##  Find the  source code and try to build the tarFile 
-    ##  ------------------------------------------------------------------
-    ## FIXME: instead of warn ask or backup or increase version number (using awk)
-    allVersions <- list.files(path=file.path(Archive),pattern=paste(pkg,".*.tar.gz",sep=""),recursive=recursive,ignore.case=1L)
-    newVersion <- paste(pkg,"_",sourceCodeVersion,".tar.gz",sep="")
-    if(match(newVersion,allVersions,nomatch=0)!=0)
-        warning("Overwriting existing packaged version ",newVersion," in directory ",Archive)
-    ## if (file.exists(file.path(Archive, freshVersion))){
-    ## message(paste("\nCopied",file.path(Archive, freshVersion),"to",file.path(Archive,"old", freshVersion),"\n"))
-    ## file.copy(file.path(Archive, freshVersion),file.path(Archive,"old", freshVersion))
-    
-    if (file.exists(file.path(SourceP,"src")) && Rcpp){
-        setwd(file.path(SourceP))
-        devtools::document()
-        ## replace routine registration file: init.c
-        sink(file.path(SourceP,"src/init.c"))
-        tools::package_native_routine_registration_skeleton(file.path(SourceP),character_only=FALSE)
-        sink(NULL)
-        setwd(file.path(Archive))
-    }
-    if (roxy && require(roxygen2)){
-        if (verbose)
-            cat("... Roxygenizing ",pkg,"\n",sep="")
-        roxygenize(SourceP)}
-    if (devel & file.exists(develR))
-        R <- develR
-    else
-        R <- file.path(R.home(), "bin", "R")
+    # {{{ Find the  source code and remove so file
 
-    buildCMD <- paste(R,
-                      " CMD build",
-                      if(vignettes != TRUE){' --no-build-vignettes'},
-                      SourceP)
-    
-    if (verbose>0)
-        message("\nRefreshing (building) the package from the directory ",SourceP," via the command:\n\n",buildCMD,"\n")
-    
-    try(bbb <- system(buildCMD,intern=(verbose<2)),silent=TRUE)
-    
-    if (length(bbb)==0){
-        cat("No such file or directory: ",SourceP)
+    ## FIXME: instead of warn ask or backup or increase version number (using awk)
+    ## allVersions <- list.files(path=file.path(Archive),pattern=paste(pkg,".*.tar.gz",sep=""),recursive=recursive,ignore.case=1L)
+    ## newVersion <- paste(pkg,"_",sourceCodeVersion,".tar.gz",sep="")
+    ## if(match(newVersion,allVersions,nomatch=0)!=0)
+        ## warning("Overwriting existing packaged version ",newVersion," in directory ",Archive)
+    ## check for so files
+    sofile <- list.files(path=file.path(SourceP,"src"),pattern=paste(pkg,"\\.so$",sep=""),ignore.case=1L,full.names=1L)
+    if (length(sofile)>0){
+        for (so in sofile){
+            if (verbose) message("Remove file:",so)
+            file.remove(sofile)
+        }
     }
-    else if (length(bbb)<9){
-        cat("\n",rep("-",42),"\nMessages from build command\n",rep("-",42),"\n\n",sub("\\*","\\\n*",bbb),sep="")
-    }
+    ## setwd(file.path(SourceP))
+    ## sink(file.path(SourceP,"src/init.c"))
+    ## tools::package_native_routine_registration_skeleton(file.path(SourceP),character_only=FALSE)
+    ## sink(NULL)
+    ## setwd(file.path(Archive))
+    ## }
+    ## if (roxy && require(roxygen2)){
+
+    # }}}
+    # {{{ devtools::document
+    setwd(file.path(SourceP))   
+    cat("\nRunning devtools::document ...\n")
+    devtools::document()
+
+    ## setwd(file.path(Archive))
+
+    cat("\nRunning devtools::build ...\n")
+    devtools::build()
+
+    ## if (devel & file.exists(develR))
+        ## R <- develR
+    ## else
+        ## R <- file.path(R.home(), "bin", "R")
+    ## buildCMD <- paste(R,
+                      ## " CMD build",
+                      ## if(vignettes != TRUE){' --no-build-vignettes'},
+                      ## SourceP)
+    ## if (verbose>0)
+        ## message("\nRefreshing (building) the package from the directory ",SourceP," via the command:\n\n",buildCMD,"\n")
+    
+    ## try(bbb <- system(buildCMD,intern=(verbose<2)),silent=TRUE)
+    
+    ## if (length(bbb)==0){
+    ## cat("No such file or directory: ",SourceP)
+    ## }
+    ## else if (length(bbb)<9){
+    ## cat("\n",rep("-",42),"\nMessages from build command\n",rep("-",42),"\n\n",sub("\\*","\\\n*",bbb),sep="")
+    ## }
 
     # }}}
     # {{{  Unloading  
 
-    try(detach(pos=match(paste("package", pkg, sep = ":"),
-                         search(),
-                         nomatch=FALSE),unload=TRUE),silent=TRUE)
-    
-    
-    try(unloadNamespace(pkg),silent=TRUE)
-    dynname <- paste(file.path(lib),"/",pkg,sep="")
-    message(paste("try unloading ",dynname))
-    try(library.dynam.unload(pkg,dynname),silent=TRUE)
-    #  TRUE(detach(paste("package", pkg, sep = ":")),silent=T)
-    
-    setwd(oldPwd)
-
+    ## try(detach(pos=match(paste("package", pkg, sep = ":"),
+                         ## search(),
+                         ## nomatch=FALSE),unload=TRUE),silent=TRUE)
+    ## try(unloadNamespace(pkg),silent=TRUE)
+    ## dynname <- paste(file.path(lib),"/",pkg,sep="")
+    ## message(paste("try unloading ",dynname))
+    ## try(library.dynam.unload(pkg,dynname),silent=TRUE)
+     ## TRUE(detach(paste("package", pkg, sep = ":")),silent=T)
     # }}}
     # {{{ R-version specific install command
 
-    ## check for so files
-    ## sofile <- list.files(path=file.path(Source,"src"),pattern=paste(pkg,"\\.so$",sep=""),ignore.case=1L)
-    ## if (length(sofile)>0){
-    ## for (so in sofile){
-    ## if (verbose) message("Remove file:",so)
-    ## system(paste("rm -rf",so),intern=(verbose<2))
-    ## }
-    ## }
     ## check for lockfile
     lock <- paste(lib,"/00LOCK-",pkg,sep="")
     if (file.exists(lock)){
@@ -173,18 +152,16 @@ build <- function(pkg,
     if (verbose){
         cat("\n",rep("-",42),"\nInstalling the ",ifelse(is.na(SourceP),"selected","new")," version of ",pkg,"\n",rep("-",42),"\n\n",sep="")
     }
-    run.install <- paste(R,
-                         "CMD INSTALL",
-                         if(docs != TRUE){'--no-docs'},
-                         " -l ",
-                         file.path(lib),
-                         file.path(Archive, newVersion))
-    message(run.install)
-    system(run.install,intern=(verbose<2))
-    require(pkg, character.only = TRUE)
-
+    cat("\nRunning devtools::install ...\n")
+    devtools::install()
+    ## run.install <- paste(R,"CMD INSTALL",if(docs != TRUE){'--no-docs'}," -l ",file.path(lib),file.path(Archive, newVersion))
+    ## message(run.install)
+    ## system(run.install,intern=(verbose<2))
+    ## require(pkg, character.only = TRUE)
     # }}}
+    cat("\nCurrently installed version of",pkg,":\n")
+    packageVersion(pkg)
+    setwd(oldPwd)
 }
-
 
 
--- R/install.R
+++ R/install.R
@@ -3,7 +3,7 @@
 ##' This is a wrapper for install.packages which allows the user to forget the quotes.
 ##' Also, the package is loaded after installation.
 ##' @title Install an R package
-##' @aliases rforge 
+##' @aliases rforge install
 ##' @param pkg Quoted or unquoted name of the package
 ##' @param ... passed to install.packages
 ##' @return see install.packages 
@@ -16,3 +16,5 @@ install <- function(pkg,...){
     install.packages(as.character(substitute(pkg)),...)
     require(as.character(substitute(pkg)), character.only = TRUE)
 }
+##' @export 
+Install <- install
