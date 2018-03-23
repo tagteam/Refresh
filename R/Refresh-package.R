7973c803d8eeb8ccb6b55227a180a4fdbbcdd28d
Thomas Alexander Gerds <tag@biostat.ku.dk>
2018-03-23 17:14:53 +0100

    fixed checks

---
+++ R/Refresh-package.R
@@ -0,0 +1,28 @@
+### Refresh-package.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Mar 23 2018 (14:44) 
+## Version: 
+## Last-Updated: Mar 23 2018 (17:11) 
+##           By: Thomas Alexander Gerds
+##     Update #: 10
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+#' This is the Refresh package to ease R-package development  
+#' 
+#' @docType package
+#' @title Refresh-package
+#' @name Refresh-package
+#' @importFrom grDevices dev.off pdf
+#' @importFrom graphics par plot text
+#' @importFrom utils example install.packages packageVersion select.list
+NULL
+
+######################################################################
+### Refresh-package.R ends here
